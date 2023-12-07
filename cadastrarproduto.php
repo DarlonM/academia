bad40b5106109328ef9aade82547bf4cb419bbfd
Darlon Martins <martinsdarlon@gmail.com>
2023-12-07 09:39:14 -0300

    init

--- cadastrarproduto.php
+++ cadastrarproduto.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastrar</title>
+    <title>Cadastrar Produto</title>
     <link  rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
@@ -12,21 +12,20 @@
       
     ?>
 
-     <h2>Cadastrar</h2><br><br>
+     <h2>Cadastrar Produto</h2><br><br>
 
      <form action="assets/db/cadastrarproduto.php" method="POST">
-        <label for="nome">Nome</label><br><br>
-        <input type="text" name="nome"><br><br>
-        <label for="email">E-mail</label><br><br>
-        <input type="email" name="email"><br><br>
-        <label for="senha">Senha</label><br><br>
-        <input type="password" name="senha" minlength="3"><br><br>
-        <label for="Telefone">Telefone</label><br><br>
-        <input type="text" name="Telefone"><br><br>
-        <label for="Endereco">Endereço</label><br><br>
-        <input type="text" name="Endereco"><br><br>
-        <input type="submit" value="Enviar"><br><br>
+        <label for="Descriçaõ">Descrição</label><br><br>
+        <input type="text" Descriçaõ="Descrição"><br><br>
+        <label for="Quantidade">Quantidade</label><br><br>
+        <input type="text" Quantidade="Quantidade"><br><br>
+        <label for="Preço Unitário">Preço Unitário</label><br><br>
+        <input type="text" Preço Unitário="Preço Unitário"><br><br>
+        <label for="Preço Total">Preço Total</label><br><br>
+        <input type="text" Preço Total="Preço Total"><br><br>
+        <input type="submit" value="Cadastrar">
 
+   
     </form>
 
     <?php
