1134b10f81c77131cc7434304082ae1b0ce043df
Darlon Martins <martinsdarlon@gmail.com>
2023-12-07 11:11:43 -0300

    init

--- cadastrarproduto.php
+++ cadastrarproduto.php
@@ -17,6 +17,8 @@
      <form action="assets/db/cadastrarproduto.php" method="POST">
         <label for="Descriçaõ">Descrição</label><br><br>
         <input type="text" Descriçaõ="Descrição"><br><br>
+        <label for="Descriçaõ">Código</label><br><br>
+        <input type="text" Código="Código"><br><br>
         <label for="Quantidade">Quantidade</label><br><br>
         <input type="text" Quantidade="Quantidade"><br><br>
         <label for="Preço Unitário">Preço Unitário</label><br><br>
@@ -24,7 +26,7 @@
         <label for="Preço Total">Preço Total</label><br><br>
         <input type="text" Preço Total="Preço Total"><br><br>
         <input type="submit" value="Cadastrar">
-        
+
 
    
     </form>
